aa1ebfeb96b7e756c62a21e975aa06b3e448ab42
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-10-18 17:10:58 +0100

    All vars for cyclist

--- cyclist.hs
+++ cyclist.hs
@@ -1,11 +1,12 @@
 module Cyclist where
 
-data Cyclist = Cyclist {max10 :: Double, distance :: Double}
+data Cyclist = Cyclist {max10 :: Double, s_m :: Double, e_rem :: Double, c_b :: Double, c_t :: Double, breakaway :: Bool, speed :: Double, distance :: Double}
 
 instance Eq Cyclist where
          a == b = (distance a == distance b)
          a /= b  = (distance a /= distance b)
 
+-- orders from last to first in the race.
 instance Ord Cyclist where
          compare a b = compare (distance a) (distance b)
          a < b = (distance a) < (distance b)
@@ -18,3 +19,4 @@ instance Ord Cyclist where
          min a b
              | a < b = a
              | otherwise = b
+
