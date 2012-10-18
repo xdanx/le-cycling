31379267420548e20c91cb8783c9a7dd02bc4340
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-10-18 18:43:44 +0100

    fix

--- Pack.hs
+++ Pack.hs
@@ -16,8 +16,8 @@ getPacks cyclists =
     addToPackList :: [Pack] -> Cyclist -> [Pack]
     addToPackList [] c = 
       [Pack [c]]
-    addToPackList ((Pack []):ps) c =
-      (Pack [c]):ps
+    {-addToPackList ((Pack []):ps) c =
+      (Pack [c]):ps-}
     addToPackList ((Pack (h:cs)):ps) c 
-      | (distance h) - (distance c) < 3 = ((Pack (c:h:cs)):ps)
+      | abs((distance h) - (distance c)) < 3 = ((Pack (c:h:cs)):ps)
       | otherwise = (Pack [c]):(Pack (h:cs)):ps
