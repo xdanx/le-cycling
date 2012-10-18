0cd47872e3e22cd9e3ac5f440265a9bc3c1a65af
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-10-18 18:45:57 +0100

    removed useless patern match

--- Pack.hs
+++ Pack.hs
@@ -16,8 +16,6 @@ getPacks cyclists =
     addToPackList :: [Pack] -> Cyclist -> [Pack]
     addToPackList [] c = 
       [Pack [c]]
-    {-addToPackList ((Pack []):ps) c =
-      (Pack [c]):ps-}
     addToPackList ((Pack (h:cs)):ps) c 
       | (distance c) - (distance h) < 3 = ((Pack (c:h:cs)):ps)
       | otherwise = (Pack [c]):(Pack (h:cs)):ps
