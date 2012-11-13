eb53501925ae9573f75f7423b3b41c79ee31cd31
César Prouté <cesar.proute10@imperial.ac.uk>
2012-11-13 20:01:07 +0000

    started parsing

--- Parser.hs
+++ Parser.hs
@@ -3,6 +3,7 @@ module Parser where
 import Control.Monad.Random
 import Control.Monad.Trans
 import Data.List
+import Data.List.Split
 
 import Cyclist
 import Simulation
@@ -11,7 +12,8 @@ genRace :: String -> IO Race
 genRace n = do
   f <- readFile n 
   g <- getStdGen
-  runRandT g parse
+  (r, _) <- runRandT (parse f) g
+  return r
 
 parse :: String -> RandT StdGen IO Race
 parse [] = error "Empty parse file"
@@ -19,9 +21,13 @@ parse f = do
   let h:c = lines f
       len = read h :: Int
   cs <- sequence . map parseLine $ c
-  let (s, r) = partition (\c -> len - distance c < 5000) cs 
+  let (s, r) = partition (\c -> fromIntegral len - distance c < 5000) . concat $ cs 
   return (Race len 0 r s [])
                 
-parseLine :: String -> RandT StdGen IO Cyclist
+parseLine :: String -> RandT StdGen IO [Cyclist]
 parseLine l = do
+    let [teams, profile, attrs] = splitOn "|" l
+    return []  
+
+
 
