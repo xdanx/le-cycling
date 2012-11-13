1dca19e47b8a77c86d08305775c68cb95be91d14
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-11-13 19:09:12 +0000

    skeleton parser

---
+++ Parser.hs
@@ -0,0 +1,10 @@
+module Parser where
+
+import Cyclist
+import Simulation
+
+genRace :: String -> IO Race
+genRace n = readFile n >>= return . parse
+
+parse :: String -> Race
+parse x = Race 0 0 [] [] []
