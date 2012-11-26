44991f0cf47fa6cad3f84c6c33e2d8790583b101
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-11-26 16:34:29 +0000

    Fully moved to using Data.Unique

--- Cyclist.hs
+++ Cyclist.hs
@@ -4,6 +4,7 @@ import Control.Monad
 import Control.Monad.Random
 import Control.Monad.State
 import Control.Monad.Trans
+import Data.Unique
 import System.Random
 
 import Utils
@@ -47,19 +48,18 @@ instance Ord Cyclist where
              | a < b = a
              | otherwise = b
 
-genCyclist :: Int -> Population -> RandT StdGen (StateT Int IO) Cyclist
+genCyclist :: Int -> Population -> RandT StdGen IO Cyclist
 genCyclist team_n stats = do
            _max10 <- normal . max10s $ stats
            _genCProb <- normal . coops $ stats
            _teamCProb <- normal . coops $ stats
-           i <- lift get
-           lift . put $ (i + 1)
+           i <- liftIO (newUnique >>= return . hashUnique)
            return Cyclist {Cyclist.id = i, max10 = _max10, speedM10 = exp 2.478, tExh = (1/0), genCProb = _genCProb, teamCProb = _teamCProb, breakaway = 0, speed = 0, distance = 0, team = team_n, teamCoop = True, genCoop = True}
 
-genCyclists :: Int -> Int -> Population -> RandT StdGen (StateT Int IO) [Cyclist]
+genCyclists :: Int -> Int -> Population -> RandT StdGen IO [Cyclist]
 genCyclists n_teams team_size stats = concatMapM (\t -> replicateM team_size (genCyclist t stats)) [0..(n_teams-1)]
 
 genCyclistsIO :: Int -> Int -> Population -> IO [Cyclist]
 genCyclistsIO n_teams team_size stats = do
               g <- getStdGen
-              flip evalStateT 0 . flip evalRandT g $ genCyclists n_teams team_size stats
+              flip evalRandT g $ genCyclists n_teams team_size stats
