25342086445ad198d512e7388fd34895d94900fa
César Prouté <cesar.proute10@imperial.ac.uk>
2012-12-18 22:57:34 +0100

    finished setPackSpeed I think

--- Simulation.hs
+++ Simulation.hs
@@ -138,7 +138,10 @@ doBreakaway p = do return [p]
   
   
 setPackSpeed :: Pack -> Pack
-setPackSpeed pack@(Pack tLead l p pid) = undefined -- deal with later
+setPackSpeed pack@(Pack tLead l p pid) = packMap (\c -> c{speed = newSpeed}) pack
+  where
+    newSpeed = 1.76777 * ppped * (tanh ((atanh (0.565685*(speed l) / ppped)) + 0.538748*ppped))
+    ppped = sqrt (0.8 * (avgpmax (l <| p)))
 setPackSpeed pack@(Breakaway p t pid) = packMap (\c -> c{speed = newSpeed}) pack
   where 
     newSpeed = 1.76777 * bpped * (tanh ((atanh (0.565685*(speed someCyclist) / bpped)) + 0.538748*bpped))
