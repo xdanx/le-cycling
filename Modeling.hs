fbab97175bb34487132783e64c86fbdf739866ec
César Prouté <cesar.proute10@imperial.ac.uk>
2012-12-28 18:54:45 +0100

    split setPackSpeed in Pack/Breakaway

--- Modeling.hs
+++ Modeling.hs
@@ -7,19 +7,16 @@ import Cyclist
 import Pack
 import RungeKutta
 
+-- Need some monadic shit for new pack ids
 setPackSpeed :: Pack -> [Pack]
-setPackSpeed p = undefined
+setPackSpeed (Pack tLead l p uid) = undefined
   where
-    coef = if isBreak p then 0.9 else 0.8
-    cyclists = case p of
-      Pack _ l p' _ -> sortBy (\x y -> compare (pmax x) (pmax y)) (l <| p')
-      Breakaway p' _ _ -> sortBy (\x y -> compare (pmax x) (pmax y)) p'
+    cyclists = sortBy (\x y -> compare (pmax x) (pmax y)) (l <| p)
     avgs = Sequence.zipWith (/) (Sequence.scanl1 (+) (fmap pmax cyclists)) (fromList [1..])
-    (drop, stay) = Sequence.partition (\(c,pm) -> (pmax c) < (coef * pm)) (Sequence.zip cyclists avgs)
-    newCyclists = (fmap (\c -> updateCyclistSpeed c (coef * (pmax c))) (fmap fst stay))
-    newPack = case p of
-      Breakaway _ t i -> Breakaway newCyclists t i
-      Pack t _ _ i    -> undefined
+    (drop, stay) = Sequence.partition (\(c,pm) -> (pmax c) < (0.8 * pm)) (Sequence.zip cyclists avgs)
+    newCyclists = (fmap (\c -> updateCyclistSpeed c (0.8 * (pmax c))) (fmap fst stay))
+    newPack = undefined
+setPackSpeed (Breakaway p t uid) = undefined
 
 avgpmax :: (Seq Cyclist) -> Double
 avgpmax p = (Fold.foldl (+) 0 (fmap pmax p)) / (fromIntegral $ Sequence.length p)
