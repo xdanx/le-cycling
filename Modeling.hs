d10f1a954bfe884b82a85fd855c096ac9d833ba4
César Prouté <cesar.proute10@imperial.ac.uk>
2012-12-28 19:03:40 +0100

    added RandT StdGen IO to setPackSpeed

--- Modeling.hs
+++ Modeling.hs
@@ -2,20 +2,24 @@ module Modeling where
 
 import Data.Foldable as Fold
 import Data.Sequence as Sequence
+import Control.Monad
+import Control.Monad.Random
+
 
 import Cyclist
 import Pack
 import RungeKutta
 
 -- Need some monadic shit for new pack ids
-setPackSpeed :: Pack -> [Pack]
-setPackSpeed (Pack tLead l p uid) = undefined
+setPackSpeed :: Pack -> RandT StdGen IO [Pack]
+setPackSpeed (Pack tLead l p uid) = do undefined
   where
     cyclists = sortBy (\x y -> compare (pmax x) (pmax y)) (l <| p)
-    avgs = Sequence.zipWith (/) (Sequence.scanl1 (+) (fmap pmax cyclists)) (fromList [1..])
+    avgs = Sequence.zipWith (/) (Sequence.scanl1 (+) (fmap pmax cyclists)) (Sequence.fromList [1..])
     (drop, stay) = Sequence.partition (\(c,pm) -> (pmax c) < (0.8 * pm)) (Sequence.zip cyclists avgs)
     newCyclists = (fmap (\c -> updateCyclistSpeed c (0.8 * (pmax c))) (fmap fst stay))
     newPack = undefined
+    dropPack = undefined
 setPackSpeed (Breakaway p t uid) = undefined
 
 avgpmax :: (Seq Cyclist) -> Double
