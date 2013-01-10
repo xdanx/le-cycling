d78506f05346597b33fe78bb3e4eee30720d907e
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2013-01-10 20:52:58 +0000

    more tests

--- Checks.hs
+++ Checks.hs
@@ -18,7 +18,6 @@ instance Arbitrary Cyclist where
   arbitrary = MkGen $ \x y -> unsafePerformIO . flip evalRandT x $ (genCyclist 1 (standardCoop, avg))
   shrink = (:[])
 
-
 instance Arbitrary Pack where
   arbitrary = do
     a@(l:p) <- listOf1 arbitrary
@@ -35,3 +34,27 @@ instance Arbitrary Race where
                    sprinters <- listOf1 arbitrary
                    return $ Race 0 10000 packs sprinters []
          shrink = (:[])
+
+lenPack :: Pack -> Int
+lenPack = Sequence.length . getPack
+
+lenRace :: Race -> Int
+lenRace (Race _ _ runners sprinters win ) = (sum . map lenPack $ runners) + Prelude.length sprinters + Prelude.length win
+
+testListCyclist :: ([Cyclist] -> [Cyclist]) -> [Cyclist] -> Bool
+testListCyclist f l = (Prelude.length (f l) == Prelude.length l)
+
+testPack :: (Pack -> Pack) -> Pack -> Bool
+testPack f l = (lenPack $ l) == (lenPack . f $ l)
+
+testListPack :: ([Pack] -> [Pack]) -> [Pack] -> Bool
+testListPack f l = (sum . map lenPack $ l) == (sum . map lenPack . f $ l)
+
+testRace :: (Race -> Race) -> Race -> Bool
+testRace f l = (lenRace l) == (lenRace . f $ l)
+
+testRaceM :: (Race -> RandT StdGen IO Race) -> Race -> Bool
+testRaceM f l = (lenRace l) == (lenRace . unsafePerformIO . flip evalRandT g . f $ l)
+          where g = unsafePerformIO $ getStdGen
+
+-- all testEq1 []
