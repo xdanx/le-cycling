bc74b6ca88af1a7e757d21144830afe1450f1822
César Prouté <cesar.proute10@imperial.ac.uk>
2013-01-10 19:55:31 +0000

    arb

--- Checks.hs
+++ Checks.hs
@@ -4,5 +4,17 @@ import Test.QuickCheck
 
 import Simulation
 import Pack
+import Cyclist
 
+
+instance Arbitrary Cyclist where
+  arbitrary = 
+    _uid <- undefined
+    _pmax <- choose (0, 2.6)
+    _ 
+    Cyclist {uid = ,
+             pmax = choose ()}
+  
+  
+  shrink = :[]
 
